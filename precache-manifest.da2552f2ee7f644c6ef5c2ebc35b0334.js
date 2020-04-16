6055f00fed6c07a10f3d9fa4a102bdba13343111
mpaczko <michalpaczko@gmail.com>
2020-04-16 11:47:11 +0200

    Updates

---
+++ precache-manifest.da2552f2ee7f644c6ef5c2ebc35b0334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6248dc56fafdc8a2ce0a299754b5351",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "0f6425663de77b74d14b",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "30d0eda4c6200fc3652c",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6425663de77b74d14b",
+    "url": "/react-deploy/static/js/main.a6b33c6b.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  }
+]);
